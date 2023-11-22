a81430fb07f7eb24fc26c6945193458ad23cb580
JF-Tahiti <151681266+JF-Tahiti@users.noreply.github.com>
2023-11-22 00:50:59 +0000

    first commit

---
+++ calculator.py
@@ -0,0 +1,65 @@
+
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return x / y
+
+
+def square(x, y):
+    return x*x, y*y
+    
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+print("5.Square")
+
+while True:
+    # take input from the user
+    choice = input("Enter choice(1/2/3/4/5): ")
+
+    # check if choice is one of the four options
+    if choice in ('1', '2', '3', '4', '5'):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        
+        elif choice == '5':
+            print(num1, "*", num1, "=", square(num1, num1))
+            print(num2, "*", num2, "=", square(num2, num2))
+
+
+
+        # check if user wants another calculation
+        # break the while loop if answer is no
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+    else:
+        print("Invalid Input")
