b7eec9f6ae261e70ef9e788a90785677c4d700f1
JF-Tahiti <151681266+JF-Tahiti@users.noreply.github.com>
2023-11-22 02:50:31 +0000

    [DEV] add q for quit program

--- calculator.py
+++ calculator.py
@@ -24,16 +24,20 @@ print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
 print("5.Square")
+print("q to exit")
 
 while True:
-    # take input from the user
-    choice = input("Enter choice(1/2/3/4/5): ")
+    
+    choice = input("Enter choice(1/2/3/4/5/q): ")
+    choice = input("Give me an operator or \"exit\" to stop : ")
 
-    # check if choice is one of the four options
+    if choice == "q":
+        break
+
     if choice in ('1', '2', '3', '4', '5'):
         try:
-            num1 = float(input("Enter first number: "))
-            num2 = float(input("Enter second number: "))
+            num1 = int(input("Enter first number: "))
+            num2 = int(input("Enter second number: "))
         except ValueError:
             print("Invalid input. Please enter a number.")
             continue
@@ -55,9 +59,6 @@ while True:
             print(num2, "*", num2, "=", square(num2, num2))
 
 
-
-        # check if user wants another calculation
-        # break the while loop if answer is no
         next_calculation = input("Let's do next calculation? (yes/no): ")
         if next_calculation == "no":
           break
